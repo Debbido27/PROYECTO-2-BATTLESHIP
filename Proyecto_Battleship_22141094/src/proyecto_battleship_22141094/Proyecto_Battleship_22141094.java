3d7deecca9995e19dd6d6f5ed206b1ebb8bca4e1
Debbido27 <davidmedina272004@gmail.com>
2026-01-25 18:16:56 -0600

    Se agrego el menu 1 principal de main

--- Proyecto_Battleship_22141094/src/proyecto_battleship_22141094/Proyecto_Battleship_22141094.java
+++ Proyecto_Battleship_22141094/src/proyecto_battleship_22141094/Proyecto_Battleship_22141094.java
@@ -1,10 +1,46 @@
 
 package proyecto_battleship_22141094;
 
+import java.util.Scanner;
+
 public class Proyecto_Battleship_22141094 {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+ 
+        String opMenu1;
+      
+        do{
+            System.out.println("Bienvenido a menu Principal");
+            System.out.println("1. Login: ");
+            System.out.println("2. Crear player: ");
+            System.out.println("3. Salir: ");
+            System.out.println("Ingrese una opcion: ");
+            opMenu1=entrada.next();
+            
+            switch(opMenu1){
+             
+            case "1":
+                System.out.println("Bienvenido a login");
+            break;
+            
+            case "2":
+                System.out.println("Bienvenido a crear player");
+            break;
+            
+            case "3":
+                System.out.println("Gracias por usar el programa");
+            break;
+            
+            default:
+                System.out.println("INGRESE UNA OPCION CORRECTA!!");
+            break;
+            }
+        
+        
+        }while(!opMenu1.equals("3"));
+         
         
     }
     
