46d0a5db1fc05208bf4948dab27d77d5430dbd91
Debbido27 <davidmedina272004@gmail.com>
2026-02-08 20:11:00 -0600

    Se agrego las variales, y el constructor en tablero logico, las cuales son los arreglos de barcos disparos, y las pocisoines

--- Proyecto_Battleship_22141094/src/proyecto_battleship_22141094/TableroLogico.java
+++ Proyecto_Battleship_22141094/src/proyecto_battleship_22141094/TableroLogico.java
@@ -2,8 +2,37 @@
 package proyecto_battleship_22141094;
 import Barcos.BARCOS;
 
-
-
 public class TableroLogico {
+private int filas;
+private int columnas;
+
+private BARCOS [][] tableroBarcos;  
+private char [][] tableroDisparos;
+private char ultimoTipoImpactado;
+private boolean ultimoBarcoHundido;
+private BARCOS [] barcos;
+private int [] barcoFila;
+private int [] barcoColumna;
+private boolean [] barcoHorizontal;
+private int totalBarcos;
+
+static final char AGUA = 'A';
+static final char IMPACTO = 'X';
+static final char FALLO ='F';
+
+public TableroLogico (int filas, int columnas){
+    this.filas=filas;
+    this.columnas=columnas;
+    this.tableroBarcos= new BARCOS [filas][columnas];
+    this.tableroDisparos= new char [filas][columnas];
+    this.barcos= new BARCOS[10];
+    this.barcoFila=new int [10];
+    this.barcoColumna=new int [10];
+    this.barcoHorizontal = new boolean[10];
+    this.totalBarcos=0;
+    
+}
+
+
     
 }
