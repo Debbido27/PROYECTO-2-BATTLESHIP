82e64b67cbcbe7bc9f368853234208e71106ccc7
Debbido27 <davidmedina272004@gmail.com>
2026-02-12 02:39:31 -0600

    Se agrego un enum y interfaz contrato para los avisos

--- Proyecto_Battleship_22141094/src/IMPRESIONES/TABLERO_VISUAL.java
+++ Proyecto_Battleship_22141094/src/IMPRESIONES/TABLERO_VISUAL.java
@@ -16,5 +16,33 @@ public class TABLERO_VISUAL {
     private boolean modoTutorial;
     private Scanner scanner;    
     
+    //ENUM FASES DE JUEGO
+        private enum FaseJuego {
+        CONECTANDO_PLAYER2,
+        COLOCANDO_PLAYER1,
+        COLOCANDO_PLAYER2,
+        EN_JUEGO,
+        TERMINADO
+    }
     
+    
+    private FaseJuego faseActual;
+    private int barcosColocadosPlayer1;
+    private int barcosColocadosPlayer2;
+    private boolean turnoPlayer1;
+    private String barcoSeleccionadoCodigo = "PA";
+    private int tamanoBarcoSeleccionado = 5;
+    private boolean horizontalSeleccionado = true;
+    
+    //INTERFAZ CONTRATO PARA AVISAS
+    public interface Avisos {
+        void avisar(String aviso, boolean error);
+        void avisarFin(String winner);
+        void avisarVolverMenu();
+    }
+    
+    
+    private Avisos listener;
 }
+
+
