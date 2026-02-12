e8e41ba9267f1a73ea3b071ae0e3295302003060
Debbido27 <davidmedina272004@gmail.com>
2026-02-12 01:24:40 -0600

    Se creo la clase battleship donde se imprimira el menu principal

---
+++ Proyecto_Battleship_22141094/src/IMPRESIONES/Battleship.java
@@ -0,0 +1,13 @@
+
+package IMPRESIONES;
+
+import java.util.Scanner;
+import proyecto_battleship_22141094.CONFIGURACION;
+import proyecto_battleship_22141094.LoginManager;
+
+public class Battleship {
+    private static LoginManager loginManager = new LoginManager();
+    private static CONFIGURACION configGlobal = new CONFIGURACION();
+    private static String usuarioActual = null;
+    private static Scanner entrada = new Scanner (System.in);
+}
