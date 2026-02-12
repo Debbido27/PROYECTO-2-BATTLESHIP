79a2bdf3bfd0b1796ed027d1e232909c1c251819
Debbido27 <davidmedina272004@gmail.com>
2026-02-12 02:33:37 -0600

    Inicio clase tablero en consola

--- Proyecto_Battleship_22141094/src/IMPRESIONES/TABLERO_VISUAL.java
+++ Proyecto_Battleship_22141094/src/IMPRESIONES/TABLERO_VISUAL.java
@@ -1,7 +1,20 @@
 
 package IMPRESIONES;
 import Barcos.BARCOS;
+import java.util.Scanner;
+import proyecto_battleship_22141094.LoginManager;
+import proyecto_battleship_22141094.TableroLogico;
 
 public class TABLERO_VISUAL {
+    private TableroLogico tableroLogicoPlayer1;
+    private TableroLogico tableroLogicoPlayer2;
+    private LoginManager loginManager;
+    
+    private String player1Username;
+    private String player2Username;
+    private int dificultad;
+    private boolean modoTutorial;
+    private Scanner scanner;    
+    
     
 }
